4f42f0c367e6486a5b20c0651640bc9fe3125fc7
Kenneth Espaldon <kenespaldon@gmail.com>
2026-07-30 08:51:37 -0700

    refactor: expose AuthenticationManager for dependency injection

--- backend/src/main/java/com/dinosoar/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/dinosoar/backend/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.dinosoar.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -31,5 +33,10 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfiguration) {
+        return authConfiguration.getAuthenticationManager();
     }
 }
